45281ec84559718f919a3745479020904e560d1c
Ashish <ashishkumarjha9896@gmail.com>
2026-08-11 16:29:35 +0530

    Public vs Private class

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+//Private class
+#include <iostream>
+using namespace std;
+
+class Student{
+    private:
+    int rollno;
+    string name;
+    int regno;
+    void display(){
+        cout << "Roll No: " << rollno << endl;
+        cout << "Name: " << name << endl;
+        cout << "Registration No: " << regno << endl;
+    }
+    public:
+    void setData(int r, string n, int reg){
+        rollno = r;
+        name = n;
+        regno = reg;
+    }
+};
+
+int main(){
+    Student s1;
+    s1.setData(1, "Ashish", 12523453);
+    
+
+    return 0;
+}
